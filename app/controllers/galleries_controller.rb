ad8a7172bced27dbd8561cca1ba0717d7f99f8e4
david toron <david@toron.popschool>
2017-02-21 15:46:55 +0100

    localhost : ok

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,21 @@
+class GalleriesController < ApplicationController
+  before_action :set_gallery, only: [:show, :edit, :update, :destroy]
+
+  # GET /galleries
+  # GET /galleries.json
+  def index
+    @galleries = Gallery.all
+  end
+
+  # GET /galleries/1
+  # GET /galleries/1.json
+  def show
+  end
+
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_gallery
+      @gallery = Gallery.find(params[:id])
+    end
+
+end
